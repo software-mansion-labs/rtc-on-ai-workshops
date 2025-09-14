52cf87572c2a00e6ff078ce5b629159cf3b22269
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-09-14 15:05:33 +0200

    feat: implement speech to text methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,42 @@
 #include "core/audio/audio_recorder.h"
 #include "core/llm/llm_runner.h"
 #include "core/stt/speech_to_text.h"
-#include <chrono>
-#include <fstream>
 #include <iostream>
 #include <span>
-#include <string>
-#include <thread>
 #include <vector>
 
-using executorch::runtime::Error;
 using namespace rtc_runner;
 
 int main() {
-  std::cout << "LOCAL AI" << std::endl;
-  std::cout << std::string(50, '=') << std::endl;
+  std::cout << "🎙️  Speech-to-Text Test" << std::endl;
+  std::cout << std::string(30, '=') << std::endl;
+
+  AudioRecorder audioRecorder;
+  SpeechToText speechToText("models/stt/encoder.pte", "models/stt/decoder.pte",
+                            "models/stt/tokenizer.json");
+  speechToText.initialize();
+
+  if (!audioRecorder.loadFromWav(
+          "/Users/norbertklockiewicz/Desktop/work/swm_ai/"
+          "rtc-on-ai-workshops/recordings/recording_1.wav")) {
+    std::cerr << "Could not load audio file" << std::endl;
+    return 1;
+  }
+
+  auto audioData = audioRecorder.getAudioData();
+  try {
+    std::string transcription = speechToText.transcribe(audioData);
+
+    if (!transcription.empty()) {
+      std::cout << "Result: \"" << transcription << "\"" << std::endl;
+    } else {
+      std::cout << "Transcription failed - no text generated" << std::endl;
+    }
+
+  } catch (const std::exception &e) {
+    std::cerr << "Transcription error: " << e.what() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
